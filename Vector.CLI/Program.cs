549444f2e88539d8b7c615d81f0db66c7b394ff7
Oscar <oscar.thornell@gmail.com>
2021-05-24 12:37:48 +0200

    New console

---
+++ Vector.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vector.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
